b7d86f34738e3e6be7b9cbf98c168f453dbac222
AstrooBot <129632296+AstrooBot@users.noreply.github.com>
2024-02-25 16:37:59 -0500

    Upgrade modularity

--- BlackJack/Packages/Game.py
+++ BlackJack/Packages/Game.py
@@ -1,43 +1,46 @@
 import Classes
 import Rules21 
 
-count = 0
-countIA = 0
-cardPlayed = 0
-cards = Classes.ShuffleCards()
-User = Classes.Player('Jugador', count)
-Crupier = Rules21.IA('Crupier', countIA) 
+
 
 while Rules21.replay() == True:
-
-    count += Rules21.value(cards[cardPlayed])
-    cardPlayed = Rules21.takeCard('Jugador', cards[cardPlayed], count, cardPlayed)
-    count += Rules21.value(cards[cardPlayed])
-    cardPlayed = Rules21.takeCard('Jugador', cards[cardPlayed], count, cardPlayed)
+    count = 0
+    countIA = 0
+    cardPlayed = 0
+    cards = Classes.ShuffleCards()
+    User = Classes.Player('Jugador', 0)
+    Crupier = Rules21.IA('Crupier', 0)
+     
+    User.count += Rules21.value(cards[cardPlayed],User.count)
+    cardPlayed = Rules21.takeCard(User.name, cards[cardPlayed], User.count, cardPlayed)    
+    User.count += Rules21.value(cards[cardPlayed],User.count)
+    cardPlayed = Rules21.takeCard(User.name, cards[cardPlayed], User.count, cardPlayed)
     print('Empieza turno del Crupier')
-    countIA += Rules21.value(cards[cardPlayed])
+    Crupier.count += Rules21.value(cards[cardPlayed],Crupier.count)
     cardPlayed += 1
     print('Termina turno del Crupier')
     print('Empieza turno del Crupier')
-    countIA += Rules21.value(cards[cardPlayed])
+    Crupier.count += Rules21.value(cards[cardPlayed],Crupier.count)
     cardPlayed += 1
     print('Termina turno del Crupier')
-
+ 
     while cardPlayed <= 52:
         v = input("¿Continuar? Si (-s) No (-n)\n")
         if v == "s":
-            count += Rules21.value(cards[cardPlayed])
-            cardPlayed = Rules21.takeCard('Jugador', cards[cardPlayed], count, cardPlayed)       
+            User.count += Rules21.value(cards[cardPlayed],User.count)
+            cardPlayed = Rules21.takeCard(User.name, cards[cardPlayed], User.count, cardPlayed)
+            if User.count > 21:
+                break    
         elif v == "n":
             print("Has parado de jugar")
-            while countIA != 21:
-                if countIA + Rules21.value(cards[cardPlayed]) == 21:
-                    countIA += Rules21.value(cards[cardPlayed])
+            while Crupier.count != 21:
+                if Crupier.count + Rules21.value(cards[cardPlayed],Crupier.count) == 21:
+                    Crupier.count += Rules21.value(cards[cardPlayed],Crupier.count)
                 else:
                     print('Empieza turno del Crupier')
                     cardPlayed += 1
                     print('Termina turno del Crupier')
-            Rules21.winner(count, countIA)
+            Rules21.winner(User.count, Crupier.count)
             break
     print("Fin del juego\nLa casa siempre ganara, SIEMPRE")
     
